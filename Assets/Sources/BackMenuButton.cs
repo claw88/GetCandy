af776b58248ef22ef5fb4dc7465622395e572d6e
Kobayashi <kobahiro0519@gmail.com>
2020-06-03 21:56:54 +0900

    Create StartMenu

---
+++ Assets/Sources/BackMenuButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackMenuButton : MonoBehaviour
+{
+    public void OnClickBackMenuButton()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
---
+++ Assets/Sources/ExitGameButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGameButton : MonoBehaviour
+{
+    public void OnClickExitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
+    }
+}
